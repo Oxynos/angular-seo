ef9fbc7ad2fb0f2843173f9b4bd1a168b7c18391
Rohaut, Cedric (DI SO AC) - AF <cerohaut@airfrance.fr>
2020-02-07 15:41:44 +0100

    add ngx-seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SeoSocialShareService } from 'ngx-seo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-seo';
+
+  constructor(private readonly seoSocialShareService: SeoSocialShareService) {
+
+  }
 }
